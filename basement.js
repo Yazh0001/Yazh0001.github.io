8a155821b940e67ab9f5b86d70258402d99bcc77
Yazh0001 <73057307+Yazh0001@users.noreply.github.com>
2021-08-24 22:47:45 +0800

    Last commit, first day

--- basement.js
+++ basement.js
@@ -4,13 +4,22 @@ const myscreen = document.getElementById("screen")
 
 function print(x){output_text.innerHTML = x;}
 
+const gravity = 0.3
+
+const screenh = 500
+const screenw = 500
+
 var hovering = false;
 var clicking = false;
 
 var mousex = 0
+var mousexvel = 0
+var mouseyvel = 0
 var mousey = 0
 
 window.addEventListener("mousemove", e=>{
+    mousexvel = e.screenX - mousex
+    mouseyvel = e.screenY - mousey
     mousex = e.screenX 
     mousey = e.screenY 
 })
@@ -29,9 +38,9 @@ class box {
         this.height = 10
         
         this.xvel = 0
-        this.yvel = 5
+        this.yvel = 0
         this.xacc = 0
-        this.yacc = 0.03
+        this.yacc = gravity
     }
 
     resize(width, height){
@@ -46,12 +55,31 @@ class box {
         this.element.style.left = x + "px";
         this.y = y
         this.element.style.top = y + "px";
-        if(x + this.width > 1455 ){
-            this.x = 1460 - this.width
+        if(x + this.width > screenw ){
+            clicking = false
+            this.xvel = - this.xvel/2
+            this.x = screenw - this.width
             this.element.style.left = x + "px"
         }
-        if(y + this.height > 495){
-            this.y = 495 - this.height
+        if(y + this.height > screenh){
+            clicking = false
+            mybox.yacc = gravity
+            if (this.xvel > 0){this.xvel = this.xvel - 0.01}
+            if (this.xvel < 0){this.xvel = this.xvel + 0.01}
+            this.y = screenh - this.height
+            this.yvel = -this.yvel/1.3
+            this.element.style.top = y + "px"
+        }
+        if(x < 0){
+            clicking = false
+            this.xvel = - this.xvel/2
+            this.x = 0
+            this.element.style.left = x + "px"
+        }
+        if(y < 0){
+            clicking = false
+            this.yvel = 0
+            this.y = 0
             this.element.style.top = y + "px"
         }
     }
@@ -68,32 +96,43 @@ class box {
 
 mybox = new box(boxelement)
 
-mybox.resize(200, 100)
-mybox.moveto(45, 45)
+mybox.resize(100, 100)
+mybox.moveto(200, 0)
 
 setInterval(() => {
     mybox.element.addEventListener("mouseover", e =>{hovering = true})
     mybox.element.addEventListener("mouseleave", e=>{hovering = false})
     mybox.element.addEventListener("mousedown", e=>{
         if(hovering){
-        clicking = true;
-        startx = mousex;
-        starty = mousey;
-        boxstartx = mybox.x;
-        boxstarty = mybox.y; 
+        mybox.yacc = 0
+        mybox.xvel = 0
+        mybox.yvel = 0
+        clicking = true
+        startx = mousex
+        starty = mousey
+        boxstartx = mybox.x
+        boxstarty = mybox.y
         }
     })
     mybox.element.addEventListener("mouseup", e=>{
         if(hovering){
-        clicking = false 
+        clicking = false
+        mybox.xvel = Math.floor(mousexvel/3)
+        mybox.yvel = Math.floor(mouseyvel/2)
+        mybox.yacc = gravity
         }
     })
     if(clicking){
-        mybox.moveto(boxstartx + (mousex - startx), boxstarty + (mousey - starty))
+        var newx = boxstartx + (mousex - startx)
+        var newy = boxstarty + (mousey - starty)
+        
+        mybox.moveto(newx, newy)
+        
     }
-    if(!clicking){
+    else{
         mybox.move()
     }
-    // mybox.accelerate()
+    // print("Bouncy Box")
+    mybox.accelerate()
 }, 1);
 
